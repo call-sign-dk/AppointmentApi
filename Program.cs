013f6babf0b0aabe082615820542932f78e65b42
Deyanesh Krishna <deyanesh.k@disprz.com>
2025-09-17 14:48:24 +0530

    F and B integrated_Layout Problem

--- Program.cs
+++ Program.cs
@@ -1,16 +1,27 @@
 using AppointmentApi.Business;
 using AppointmentApi.Data;
 using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-//register dbcontext
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // Your React app URL
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+// Register dbcontext
 builder.Services.AddDbContext<AppointmentsDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllers();  // Add support for Controllers
 builder.Services.AddScoped<IAppointmentBL, AppointmentBL>();  // Register your BL service
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -23,11 +34,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Use CORS before other middleware
+app.UseCors("AllowReactApp");
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();  // Map controller routes
 
 app.Run();
-///Commit before resuming frontend
