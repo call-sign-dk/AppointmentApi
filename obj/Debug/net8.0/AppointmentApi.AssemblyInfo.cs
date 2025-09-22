4c86ca9eba1f86d22cf86d786ec68bc68187931c
Deyanesh Krishna <deyanesh.k@disprz.com>
2025-09-22 12:54:30 +0530

    Update_Appointment_time problem resolved

--- obj/Debug/net8.0/AppointmentApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/AppointmentApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppointmentApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b1a6a014f6e6b8a602667626bd313a842fa1f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1f1bb85504f41abc3825ed226138ad485db544e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppointmentApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppointmentApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
