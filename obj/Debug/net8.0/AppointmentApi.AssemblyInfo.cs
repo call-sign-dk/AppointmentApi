643ea767ec2cfbd6f1a842b7c38de33e91351dd9
Deyanesh Krishna <deyanesh.k@disprz.com>
2025-09-24 14:28:00 +0530

    API Call get is used and caches appointments

--- obj/Debug/net8.0/AppointmentApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/AppointmentApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppointmentApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1f1bb85504f41abc3825ed226138ad485db544e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c86ca9eba1f86d22cf86d786ec68bc68187931c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppointmentApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppointmentApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
